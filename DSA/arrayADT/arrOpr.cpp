9933224636a4721c7b318558e7ac493f39108d27
XZNONz <shivaliksingh39@gmail.com>
2023-05-18 18:23:20 +0530

    "binary search algo"

--- DSA/arrayADT/arrOpr.cpp
+++ DSA/arrayADT/arrOpr.cpp
@@ -77,6 +77,37 @@ int linearSearch(struct Array arr, int key){
     return -1;
 }
 
+int binarySearch(struct Array arr, int key){
+    int l,h,mid;
+    l=0;
+    h=arr.length-1;
+
+    while(l<=h){
+        mid=(l+h)/2;
+        if(key==arr.A[mid])
+            return mid;
+        else if(key<arr.A[mid])
+            h=mid-1;
+        else    
+            l=mid+1;
+    }
+    return -1;
+}
+
+int rBinSearch(int a[],int l,int h,int key){
+    int mid;
+    if(l<=h){
+        mid=(l+h)/2;
+        if(a[mid]==key)
+            return mid;
+        else if(key<a[mid])
+            return rBinSearch(a,l,mid-1,key);
+        else    
+            return rBinSearch(a,mid+1,h,key);
+    }
+    return -1;
+}
+
 int main(){
 
     struct Array arr={{1,2,3,4,5},10,5};
@@ -101,9 +132,13 @@ int main(){
     // append(&arr,10);
     // insert(&arr,2,45);
     // Delete(&arr,2);
-    // cout<<linearSearch(arr,5)<<endl;
-    cout<<linearSearhBetter(&arr,4)<<endl;   
 
-    display(arr);
+
+    // cout<<linearSearch(arr,5)<<endl;
+    // cout<<linearSearhBetter(&arr,4)<<endl;   
+    // cout<<binarySearch(arr,4)<<endl;
+    // cout<<rBinSearch(arr.A,0,arr.length,2)<<endl;
+        
+    // display(arr);
     return 0;
 }
